051d586b449c376dbb30daa5f72031b7fc3cd6d7
Yasutake Tomoya <ty0108.98@icloud.com>
2023-11-29 01:35:53 +0900

    fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,5 @@
 //= link_tree ../images
-<<<<<<< HEAD
-=======
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
->>>>>>> main
 //= link_tree ../builds
