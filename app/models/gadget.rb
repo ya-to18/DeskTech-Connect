dbbbde95993b3672a562ed5107c1ecc2b5ce581e
Yasutake Tomoya <ty0108.98@icloud.com>
2023-12-25 22:09:18 +0900

    #28 仕様変更のため中間テーブルの削除及び Gadgetモデルに外部キーを設置

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,6 +1,5 @@
 class Gadget < ApplicationRecord
-  has_many :post_gadgets, dependent: :destroy
-  has_many :posts, through: :post_gadgets
+  belongs_to :post
 
   enum genre: { keyboard: 0, monitor: 1 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-  has_many :post_gadgets, dependent: :destroy
-  has_many :gadgets, through: :post_gadgets
+  has_many :gadgets, dependent: :destroy
   belongs_to :user
 
   mount_uploader :image, PostImageUploader
