3c88bdcb26dcf0b0b4c724c396f209b86a01f712
Yasutake Tomoya <ty0108.98@icloud.com>
2024-01-14 20:24:28 +0900

    #44 ブランド表示で、重複して表示されるブランドを重複なしに修正

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def brand_remove_duplicate(post)
+    post = Post.find(post)
+    @brand = post.gadgets.map { |gadget| gadget.brand }.uniq
+    if @brand.include?('')
+      @brand.delete('')
+    end
+  end
 end
