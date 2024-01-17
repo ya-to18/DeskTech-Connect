acfb85a133fc6180e6c948d25bcc4c5f89e8368d
Yasutake Tomoya <ty0108.98@icloud.com>
2024-01-17 23:28:40 +0900

    #36 いいねボタン機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,22 @@
 class LikesController < ApplicationController
   before_action :require_login
-
-  def new
-  end
+  before_action :set_post
 
   def create
+    like = current_user.likes.new(post_id: @post.id)
+    like.save
+    redirect_to posts_path
   end
 
   def destroy
+    like = current_user.likes.find_by(post_id: @post.id)
+    like.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,10 @@ class Post < ApplicationRecord
   accepts_nested_attributes_for :gadgets, allow_destroy: true
 
   validates_associated :gadgets
-
   validates :gadgets, presence: true
-
   mount_uploader :image, PostImageUploader
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
