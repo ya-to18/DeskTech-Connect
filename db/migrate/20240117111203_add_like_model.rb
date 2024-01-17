673fc1f283e7885969316479666cdbcfa471752d
Yasutake Tomoya <ty0108.98@icloud.com>
2024-01-17 20:19:55 +0900

    #36 Likesモデルの追加

---
+++ db/migrate/20240117111203_add_like_model.rb
@@ -0,0 +1,10 @@
+class AddLikeModel < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_15_080442) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_111203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_15_080442) do
     t.datetime "updated_at", null: false
     t.bigint "post_id"
     t.index ["post_id"], name: "index_gadgets_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -47,5 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_15_080442) do
   end
 
   add_foreign_key "gadgets", "posts"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
