7ba7eae89a28122f2fd203930414b5715d8c9256
Yasutake Tomoya <ty0108.98@icloud.com>
2024-01-20 22:00:19 +0900

    #55 Fix:オートコンプリート時に重複して表示される候補値を削除

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -1,13 +1,13 @@
 class AutocompleteController < ApplicationController
   def brand
-    @gadgets = Gadget.where("brand like ?", "%#{params[:q]}%")
+    @gadgets = Gadget.select('DISTINCT brand').where("brand like ?", "%#{params[:q]}%")
     respond_to do |format|
       format.js
     end
   end
 
   def name
-    @gadgets = Gadget.where("name like ?", "%#{params[:q]}%")
+    @gadgets = Gadget.select('DISTINCT name').where("name like ?", "%#{params[:q]}%")
     respond_to do |format|
       format.js
     end
