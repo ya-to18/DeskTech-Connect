0071584099d991751fb9adbf9488e0ff886f0168
Yasutake Tomoya <ty0108.98@icloud.com>
2024-01-23 14:54:29 +0900

    #63 Add:routes.rbにパスワードリセット用のルートをセット

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/create'
-  get 'password_resets/edit'
-  get 'password_resets/update'
-  get 'likes/new'
-  get 'likes/create'
-  get 'likes/destroy'
   root 'tops#index'
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :password_resets, only: %i[ create edit update ]
 
   resources :autocomplete do
     get :brand, on: :collection
