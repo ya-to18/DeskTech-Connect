0a4e3a4901b0dc996f1cbd5a1774815b3e3a4517
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-03 16:14:22 +0900

    #88 Fix:マイページ追加によるbreadcrmbの修正

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,9 +1,14 @@
 crumb :root do
-  link "みんなの投稿", posts_path
+  link "HOME", my_page_path
 end
 
 crumb :show_post do |post|
   link "#{post.user.name}さんの投稿", post_path(post.id)
+  if request.referer == posts_url
+    parent :posts_index
+  else
+    parent :root
+  end
 end
 
 crumb :edit_post do |post|
@@ -16,9 +21,12 @@ crumb :new_post do
   parent :root
 end
 
+crumb :posts_index do
+  link "みんなの投稿", posts_path
+end
+
 crumb :rank_post do
   link "人気ランキング", rank_path
-  parent :root
 end
 
 crumb :liked_post do
@@ -26,30 +34,5 @@ crumb :liked_post do
 end
 
 crumb :my_post do
-  link "自分の投稿", posts_users_path
+  link "自分の投稿一覧", posts_users_path
 end
-
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
