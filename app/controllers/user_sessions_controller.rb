176d6a8c8614d83ff9b28bab9704ad39c81d0c53
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-03 15:50:38 +0900

    #88 Fix:ログイン後のURLを変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to posts_path, flash: { success: 'ログインしました' }
+      redirect_to my_page_path, flash: { success: 'ログインしました' }
     else
       flash.now[:error] = 'ログインに失敗しました。'
       render :new, status: :unprocessable_entity
