c50aeff1932441548d6932afd74b803ee909abe4
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-12 17:26:28 +0900

    #77 Add:トップページのスライド2を作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,6 @@ class TopsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
+    @recent_3_posts = Post.order(created_at: :desc).limit(4)
   end
 end
