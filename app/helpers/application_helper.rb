5e568df2936653e3f82e9059574d41429ad2308e
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-19 23:21:08 +0900

    Add:default_meta_tagsの設定追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,33 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def default_meta_tags
+    {
+      site: 'DeskTech-Connect',
+      title: '自分のデスク環境を共有するサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'DeskTech-Connectを使用すれば、自分のデスクを共有する事ができます。',
+      keywords: 'デスク,パソコン,ガジェット',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        # image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@',
+        image: image_url('ogp/twitter_ogp.png')
+      }
+    }
+  end
+
   def page_title(page_title = '')
     base_title = 'DeskTech-Connect'
 
