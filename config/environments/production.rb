74a05c4a489d156a0bd2890396d3f3b4f26885d8
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-20 17:41:58 +0900

    Add:パスワードリセット機能の本番環境用のメールアドレスの設定追加(Gmail)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "myapp_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'https://desktech-connect-ba757d0b0928.herokuapp.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,5 +99,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://desktech-connect-ba757d0b0928.herokuapp.com' }
 end
