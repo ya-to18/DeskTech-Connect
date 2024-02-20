8556d2aeebe86979f6a349916f18897c5a71d47d
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-20 18:27:58 +0900

    Fix:action_mailer.default_url_optionsをsettigs/production.ymlに設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = 'https://desktech-connect-ba757d0b0928.herokuapp.com/'
+  config.action_mailer.default_url_options = Settings.default_url_options
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
