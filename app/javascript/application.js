391f838447395c6ddbc28acd65d140209345e4cf
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-26 17:32:40 +0900

    Add:マイページの各リンクに動的なスタイルを追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require swiper/swiper-bundle.min.js
-//= require swiper.js
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@fortawesome/fontawesome-free"
