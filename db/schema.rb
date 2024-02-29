5a5b549f4714aa827037b814b93a5a7fa2bcb82b
Yasutake Tomoya <ty0108.98@icloud.com>
2024-02-29 16:06:55 +0900

    #146 change:Userテーブルのreset_password_tokenにunique制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_15_131449) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_29_065228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_15_131449) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "gadgets", "posts"
