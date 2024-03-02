9854f84df7c5258c732291ef2c2a1e46c2e0ba32
Yasutake Tomoya <ty0108.98@icloud.com>
2024-03-02 20:25:51 +0900

    #149 add:commentsコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'comments/destroy'
   root 'tops#index'
 
   resource :tops, only: [] do
