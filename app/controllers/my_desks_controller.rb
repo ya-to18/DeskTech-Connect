39f9ee7ceed83853518d63f6a0b3cab8944098aa
Yasutake Tomoya <ty0108.98@icloud.com>
2024-03-11 16:56:55 +0900

    #160 add:マイページにおけるマイデスクのCRUD機能の実装

--- app/controllers/my_desks_controller.rb
+++ app/controllers/my_desks_controller.rb
@@ -1,20 +1,55 @@
 class MyDesksController < ApplicationController
+  before_action :set_my_desk, only: %i[show edit update destroy]
   def index
-    @my_desk = current_user.my_desks
+    @my_desks = current_user.my_desks
+  end
+
+  def show
+    @gadgets = @my_desk.gadgets
   end
 
   def new
-  end
-
-  def create
+    @my_desk = MyDesk.new
   end
 
   def edit
   end
 
+  def create
+    @my_desk = current_user.my_desks.new(my_desk_params)
+    respond_to do |format|
+      if @my_desk.save
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
+    respond_to do |format|
+      if @my_desk.update(my_desk_params)
+        format.turbo_stream
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @my_desk.destroy!
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_my_desk
+    @my_desk = MyDesk.find(params[:id])
+  end
+
+  def my_desk_params
+    params.require(:my_desk).permit(:name, :user_id)
   end
 end
--- app/models/my_desk.rb
+++ app/models/my_desk.rb
@@ -1,7 +1,8 @@
 class MyDesk < ApplicationRecord
   has_many :my_desk_gadgets, dependent: :destroy
-
   has_many :gadgets, through: :my_desk_gadgets
 
   belongs_to :user
+
+  validates :name, presence: true
 end
