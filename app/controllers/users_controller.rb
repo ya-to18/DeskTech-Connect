5e423960ccfc5b2e731592a2063c9e729e429c93
Yasutake Tomoya <ty0108.98@icloud.com>
2024-04-01 16:41:28 +0900

    add:ユーザー登録時のオートログイン機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       @user.my_desks.create!(name: 'Default')
       redirect_to root_path, flash: { success: t('.success') }
     else
